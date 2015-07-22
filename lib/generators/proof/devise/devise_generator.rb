56943248d41e88667a0ae1a968da8e99d94e3aad
Thomas Hobohm <superman3275@gmail.com>
2015-07-22 16:26:14 -0500

    Fixed DeviseGenerator's description.

--- lib/generators/proof/devise/devise_generator.rb
+++ lib/generators/proof/devise/devise_generator.rb
@@ -2,7 +2,7 @@ module Proof
   module Generators
     class DeviseGenerator < Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
-      desc "This generator creates a 'login' route, and configures it to work with Devise."
+      desc "This generator creates a 'login' route, and configures it to work with Devise (with the default model name User)."
 
       def create_controller
         copy_file "authentication_controller.rb", "app/controllers/authentication_controller.rb"
